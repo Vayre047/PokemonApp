6487c3b56827cdbcc2171933dbbfe5006f67e669
Vayre047 <94454907+Vayre047@users.noreply.github.com>
2023-09-15 10:59:03 +0100

    Create Search Input

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import SearchInput from './Components/SearchInput/searchinput';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pokemon API</h1>
+      <SearchInput />
+    </div>
+  );
+}
+
+export default App;
