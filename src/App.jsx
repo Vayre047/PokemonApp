5d5a501469a148dfebe689b71f23fd20b9c60dbe
Vayre047 <94454907+Vayre047@users.noreply.github.com>
2023-09-15 13:27:34 +0100

    Shows the pokemons name

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,33 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import SearchInput from './Components/SearchInput/searchinput';
+import CardList from './Components/CardList/cardlist';
 
 function App() {
   const [searchValue, setSearchValue] = useState('');
   const [pokemons, setPokemons] = useState([]);
 
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=1000")
-    .then((response) => response.json())
-    .then((pokemon) => setPokemons(pokemon));
-  })
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=1000")
+      .then((response) => response.json())
+      .then(pokemon => setPokemons(pokemon.results));
+  });
+
 
   function searchHandler(event){
     let searchFieldValue = event.target.value.toLocaleLowerCase();
     setSearchValue(searchFieldValue);
   }
 
-  // const pokemonFiltered = pokemons.filter((pokemon) =>{
-  //   return pokemon.name.toLocaleUpperCase().includes(searchValue);
-  // });
+  const pokemonsFiltered = pokemons.filter((pokemon) =>{
+    return pokemon.name.toLocaleUpperCase().includes(searchValue);
+  });
 
   return (
     <div className="App">
       <h1>Pokemon API</h1>
       <SearchInput valueFiltered={searchHandler} placeholder="Search a Pokemon" />
+      <CardList pokemons={pokemonsFiltered} />
     </div>
   );
 }
--- src/Components/CardList/cardlist.jsx
+++ src/Components/CardList/cardlist.jsx
@@ -1,7 +1,14 @@
-function CardList(){
+function CardList(props){
+    const { pokemons } = props;
     return(
         <div>
-
+            {pokemons.map((pokemon) => {
+                return(
+                    <div>
+                        <h1>{pokemon.name}</h1>
+                    </div>                    
+                )
+            })}
         </div>
     )
 }
